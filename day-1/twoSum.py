fe54774057bac2a47e285b4ed490e2a062ac58ff
girish choudhary <girishsonu2000@gmail.com>
2021-11-12 22:06:21 +0530

    1st python code

---
+++ day-1/twoSum.py
@@ -0,0 +1,11 @@
+def twoSum(nums, target):
+        arr={}
+        for i, num in enumerate(nums):
+            pot_tar= target - num
+            if pot_tar in arr:
+                return[arr[pot_tar],i]
+            else:
+                arr[num]=i
+        return []
+
+print(twoSum(nums = [2,7,11,15], target))
